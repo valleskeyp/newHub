433cfb2195eef1038a748cb9c9ec77e7226f23f3
Peter Valleskey <player1122@gmail.com>
2012-05-03 16:25:24 -0700

    wrapping up ViewController

--- AOC2/project1/project1/ViewController.m
+++ AOC2/project1/project1/ViewController.m
@@ -17,17 +17,18 @@
 
 - (void)viewDidLoad
 {   
-    SquareClass *square = (SquareClass*)[ShapeFactory CreateShape:0];
-    int area = [square getArea];
-    NSString* name = [square getName];
-    NSLog(@"Shape %@, Area:%d", name, area);
-    
     self.view.backgroundColor = [UIColor lightGrayColor];
     field1 = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 90.0f, 320.0f, 80.0f)];
     if (field1 != nil) 
     {
+        SquareClass *square = (SquareClass*)[ShapeFactory CreateShape:0];
+        NSMutableString *combo = [[NSMutableString alloc] initWithString:@"Shape "];
+        [combo appendString:[square getName]];
+        [combo appendString:@", area:"];
+        [combo appendFormat:@"%d",[square getArea]];
+        
         field1.textColor = [UIColor blackColor];
-        field1.text = @"Default text1";
+        field1.text = combo;
         field1.backgroundColor = [UIColor lightGrayColor];
         field1.textAlignment = UITextAlignmentCenter;
         [self.view addSubview:field1];
@@ -35,8 +36,14 @@
     field2 = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 180.0f, 320.0f, 80.0f)];
     if (field2 != nil) 
     {
+        TriangleClass *triangle = (TriangleClass*)[ShapeFactory CreateShape:1];
+        NSMutableString *combo = [[NSMutableString alloc] initWithString:@"Shape "];
+        [combo appendString:[triangle getName]];
+        [combo appendString:@", area:"];
+        [combo appendFormat:@"%d",[triangle getArea]];
+        
         field2.textColor = [UIColor blackColor];
-        field2.text = @"Default text2";
+        field2.text = combo;
         field2.backgroundColor = [UIColor lightGrayColor];
         field2.textAlignment = UITextAlignmentCenter;
         [self.view addSubview:field2];
@@ -44,8 +51,14 @@
     field3 = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 270.0f, 320.0f, 80.0f)];
     if (field3 != nil) 
     {
+        RectangleClass *rectangle = (RectangleClass*)[ShapeFactory CreateShape:2];
+        NSMutableString *combo = [[NSMutableString alloc] initWithString:@"Shape "];
+        [combo appendString:[rectangle getName]];
+        [combo appendString:@", area:"];
+        [combo appendFormat:@"%d",[rectangle getArea]];
+        
         field3.textColor = [UIColor blackColor];
-        field3.text = @"Default text3";
+        field3.text = combo;
         field3.backgroundColor = [UIColor lightGrayColor];
         field3.textAlignment = UITextAlignmentCenter;
         [self.view addSubview:field3];

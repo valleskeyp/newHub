2cd808481c73e196f3f78e56cfa5bf8a4aa6a6a1
Peter Valleskey <player1122@gmail.com>
2012-05-15 10:15:02 -0700

    round 1, fight!
    aka starting project

---
+++ AOC2/project3/project3/project3/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  project3
+//
+//  Created by Peter Valleskey on 5/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
